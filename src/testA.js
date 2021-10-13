abe8db58710dd7378d8d1da4118b15a01ba96b9b
heiko <1060297977@qq.com>
2021-10-13 16:25:45 +0800

    no message

---
+++ src/testA.js
@@ -0,0 +1,5 @@
+import globalData from "./globalData";
+import myButton from "./myButton";
+import soundManager from "./soundManager";
+import itemBody from "./itemBody";
+import mainUI from "./mainUI";
